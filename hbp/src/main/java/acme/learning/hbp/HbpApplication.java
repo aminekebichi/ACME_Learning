77fca0184b6f50b7f9adc3f4d178499bf58c1b9f
Amine <amkebichi@gmail.com>
2023-12-17 15:06:19 -0500

    Initialize project using Spring Initializr

---
+++ hbp/src/main/java/acme/learning/hbp/HbpApplication.java
@@ -0,0 +1,13 @@
+package acme.learning.hbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbpApplication.class, args);
+	}
+
+}
---
+++ hbp/src/test/java/acme/learning/hbp/HbpApplicationTests.java
@@ -0,0 +1,13 @@
+package acme.learning.hbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
