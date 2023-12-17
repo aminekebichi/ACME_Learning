f85008f365d82b4c6fc51cc48651a6bd64bd0ec4
Amine Kebichi <amkebichi@gmail.com>
2023-12-17 15:14:26 -0500

    initialize controllers

---
+++ hbp/src/main/java/acme/learning/hbp/instructor/InstructorController.java
@@ -0,0 +1,9 @@
+package acme.learning.hbp.instructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/instructors")
+public class InstructorController {
+}
