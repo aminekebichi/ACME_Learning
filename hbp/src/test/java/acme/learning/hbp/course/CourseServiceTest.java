e63d1f25f492502c30b9661c9af62517205c7323
Amine Kebichi <amkebichi@gmail.com>
2023-12-17 18:24:42 -0500

    verified testing for getStudentsByCourse()

--- hbp/src/test/java/acme/learning/hbp/course/CourseServiceTest.java
+++ hbp/src/test/java/acme/learning/hbp/course/CourseServiceTest.java
@@ -3,6 +3,8 @@ package acme.learning.hbp.course;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import acme.learning.hbp.student.Student;
@@ -47,5 +49,37 @@ class CourseServiceTest {
         // Assert that the student is assigned to the course
         assertTrue(assignedCourse.getStudents().contains(sampleStudent));
         assertTrue(sampleStudent.getEnrolledCourses().contains(assignedCourse));
+    }
+
+    @Test
+    void testGetStudentsByCourse() {
+        // Create a sample course
+        Course sampleCourse = new Course();
+        sampleCourse.setId(1L);
+        sampleCourse.setName("Sample Course");
+
+        // Create sample students
+        Student student1 = new Student();
+        student1.setId(1L);
+        student1.setName("Student 1");
+
+        Student student2 = new Student();
+        student2.setId(2L);
+        student2.setName("Student 2");
+
+        // Assign students to the sample course
+        sampleCourse.setStudents(Arrays.asList(student1, student2));
+
+        // Mock the behavior of findById method
+        when(courseRepository.findById(1L)).thenReturn(Optional.of(sampleCourse));
+
+        // Call the service method
+        List<Student> students = courseService.getStudentsByCourse(1L);
+
+        // Assert the result
+        assertNotNull(students, "The list of students should not be null");
+        assertEquals(2, students.size(), "The number of students should be 2");
+        assertTrue(students.contains(student1), "Student 1 should be in the list");
+        assertTrue(students.contains(student2), "Student 2 should be in the list");
     }
 }
