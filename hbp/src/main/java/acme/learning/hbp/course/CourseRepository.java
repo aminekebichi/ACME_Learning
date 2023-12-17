f75c9cac58e8b82a85b4e9375c16c71f63dfd4ce
Amine Kebichi <amkebichi@gmail.com>
2023-12-17 15:17:40 -0500

    initialize repositories

---
+++ hbp/src/main/java/acme/learning/hbp/course/CourseRepository.java
@@ -0,0 +1,8 @@
+package acme.learning.hbp.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
