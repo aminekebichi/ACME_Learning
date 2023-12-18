38f2c97983a081989c20809572fcd6820b46d844
Amine Kebichi <amkebichi@gmail.com>
2023-12-17 22:18:12 -0500

    initialize spring security configs and initialize User entity

---
+++ hbp/src/main/java/acme/learning/hbp/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package acme.learning.hbp.config;
+
+import acme.learning.hbp.user.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+public class ApplicationConfig {
+    private final UserRepository repository;
+
+    public ApplicationConfig(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
